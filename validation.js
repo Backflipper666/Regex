5651fec63d3a55ad3a2a726ad3787a0e9a5983ba
Backflipper666 <elderly619@gmail.com>
2022-01-18 06:21:36 +0600

    logs out result of the test

--- validation.js
+++ validation.js
@@ -1,6 +1,21 @@
 // validation script here
 let reg = /[a-z]/ig;
 
+const inputs =document.querySelectorAll('input');
+
 const patterns = {
     telephone:/^\d{11}$/
 }
+
+//validation function
+function validate(field, regex) {
+
+    console.log(regex.test(field.value));
+}
+
+inputs.forEach((input) =>{
+    input.addEventListener('keyup',(e) => {
+       // console.log(e.target.attributes.name.value);
+       validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
